35be994aaf985a3b53779411022e12200d715d9c
enes.elezi <enes.elezi@commprog.com>
2026-03-16 09:08:31 +0100

    Me mengjes

--- teacher_school_erp/__manifest__.py
+++ teacher_school_erp/__manifest__.py
@@ -4,10 +4,6 @@
     'data':[
         # Security
         'security/ir.model.access.csv',
-        # Wizard
-        'views/wizards_view/assign_subject_teacher_wizard.xml',
-        # Inherited
-        'views/wizards_view/res_user_inherited.xml',
         # Views
         'views/teacher_view.xml',
         'views/student_inherit_view.xml',
