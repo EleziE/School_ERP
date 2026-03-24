a62fff380f0eb0fe29b23b8f49507137937486e1
enes.elezi <enes.elezi@commprog.com>
2026-03-24 17:25:42 +0100

    Rishiko pse nuk dalin labelat si duhet

--- students_school_erp/models/students.py
+++ students_school_erp/models/students.py
@@ -1,17 +1,37 @@
-from odoo import fields, models
+from odoo import fields, models, api, exceptions, _
 
 
 class Student(models.Model):
     _name = 'students.students'
     _description = 'Students'
 
+    # Dont know how i did it (learn python baboo se ske gja per terezi )
+    sequence = fields.Char(string='Student ID: ',
+                           readonly=True,
+                           default=lambda self: _('New'))
     user_id = fields.Many2one(comodel_name='res.users')
-    name = fields.Char(string='Name',related='user_id.name',readonly=True)
-    gender = fields.Selection(string='Gender', related='user_id.gender',readonly=True,store=True)
-    dob = fields.Date(string='Date of birth',related='user_id.dob',readonly=True)
-    blood_type = fields.Selection(string='Blood type',related='user_id.blood_type',readonly=True)
-    email = fields.Char(string='Email',related='user_id.email',readonly=True)
-    classroom_id = fields.Many2one(comodel_name='class.rooms', string='Class')
+    name = fields.Char(string='Name',
+                       related='user_id.name',
+                       readonly=True,
+                       store=True)
+    surname = fields.Char(string='Surname',
+                          related='user_id.surname',
+                          readonly=True,
+                          store=True)
+    gender = fields.Selection(string='Gender',
+                              related='user_id.gender',
+                              readonly=True)
+    dob = fields.Date(string='Date of birth',
+                      related='user_id.dob',
+                      readonly=True)
+    blood_type = fields.Selection(related='user_id.blood_type',
+                                  string='Blood Type',
+                                  readonly=True)
+    email = fields.Char(string='Email',
+                        related='user_id.email',
+                        readonly=True)
+    classroom_id = fields.Many2one(comodel_name='class.rooms',
+                                   string='Class')
     subject_id = fields.Many2many(comodel_name='subject.subject')
     state = fields.Selection(selection=[('active', 'Active'),
                                         ('inactive', 'Not Active'),
@@ -22,7 +42,8 @@ class Student(models.Model):
                              string='Status',
                              default='draft')
     suspend_reason = fields.Text(string='Suspension Reason')
-    phone = fields.Char(string='Phone no ', related='user_id.phone')
+    phone = fields.Char(string='Phone no ',
+                        related='user_id.phone')
 
     def action_open_suspend_wizard(self):
         return {
@@ -33,29 +54,39 @@ class Student(models.Model):
             'target': 'new',
             'context': {'active_id': self.id},
         }
+
+    ############################ Buttons ###########################################
     def action_draft(self):
         self.state = 'draft'
+
     def action_rejected(self):
         self.state = 'rejected'
+
     def action_graduated(self):
         self.state = 'graduated'
+
     def action_active(self):
         self.state = 'active'
+
     def action_inactive(self):
         self.state = 'inactive'
-    def action_suspended(self):
-        self.state = 'suspended'
 
+    ############################ Constraints ###########################################
+    _sql_constraints = [
+        ('unique_student_id', 'UNIQUE (sequence)', 'This Student ID already exists.'),
+        # ('unique_email', 'UNIQUE (email)', 'This Email already exists.'),
+        ('unique_user_id', 'UNIQUE (user_id)', 'This User ID already exists.'),
+        ('dob_check', 'CHECK(dob < CURRENT_DATE)', 'Date of birth must be in the past.')
+    ]
 
 
 class User(models.Model):
     _inherit = 'res.users'
 
-    dob = fields.Date(string='Date of birth')
+    surname = fields.Char(string='Surname')
     gender = fields.Selection(string='Gender',
                               selection=[('female', 'Female'),
-                                         ('male', 'Male')],
-                              required=True)
+                                         ('male', 'Male')], )
     blood_type = fields.Selection(selection=[('a+', 'A+'),
                                              ('a-', 'A-'),
                                              ('b+', 'B+'),
@@ -65,5 +96,11 @@ class User(models.Model):
                                              ('o+', 'O+'),
                                              ('o-', 'O-'),
                                              ],
-                                  string='Blood Type',
-                                  required=True)
+                                  string='Blood Type')
+    dob = fields.Date(string='Date of birth')
+
+    @api.onchange('name')
+    def _onchange_name_set_login(self):
+        if self.name:
+            username = self.name.strip().lower().replace(' ', '.')
+            self.login = f"{username}@school.com"
--- teacher_school_erp/__manifest__.py
+++ teacher_school_erp/__manifest__.py
@@ -6,7 +6,6 @@
         'security/ir.model.access.csv',
         # Views
         'views/teacher_view.xml',
-        'views/student_inherit_view.xml',
     ],
     'depends':['base_school_erp','students_school_erp'],
     'icon': 'teacher_school_erp/static/description/icon.png',
