962397149fab09d5d3433a3c9ee253a6b4b1ec45
enes.elezi <eneselezi2000@gmail.com>
2026-04-06 20:04:51 +0200

    New Module

--- my_profile_school_erp/__manifest__.py
+++ my_profile_school_erp/__manifest__.py
@@ -4,11 +4,12 @@
         'base_school_erp',
         'students_school_erp',
         'teacher_school_erp',
+        'task_school_erp',
+        'finance_school_erp',
     ],
     'data': [
         'security/ir.model.access.csv',
-        'views/my_profile_student.xml',
-        'views/my_profile_teacher.xml',
+        'views/my_profile.xml',
         'views/menu.xml',
     ],
 }
