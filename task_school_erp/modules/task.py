7a69d02d99b6e368122ee61d02a1689c85361e43
enes.elezi <enes.elezi@commprog.com>
2026-04-14 13:40:42 +0200

    Commit #1 (04/14/2026)

--- task_school_erp/modules/task.py
+++ task_school_erp/modules/task.py
@@ -28,8 +28,8 @@ class Task(models.Model):
                               compute='status_based_dates',
                               store=True,
                               group_expand='_group_expand_status')
-    starting_date = fields.Date(
-        default=fields.Date.today)
+
+    starting_date = fields.Date(default=fields.Date.today)
 
     planned_finish_date = fields.Date()
 
@@ -37,11 +37,12 @@ class Task(models.Model):
 
     description = fields.Text()
 
-    def action_in_progres(self):
-        self.status = 'in_progress'
+    check_user_finish_date = fields.Boolean(compute='_compute_check_user')
 
-    def action_completed(self):
-        self.status = 'completed'
+    @api.depends('created_for')
+    def _compute_check_user(self):
+        for rec in self:
+            rec.check_user_finish_date = (rec.created_for.user_id == self.env.user)
 
     def _group_expand_status(self, states, domain, order):
         return [key for key, val in self._fields['status'].selection]
@@ -58,22 +59,44 @@ class Task(models.Model):
             else:
                 rec.status = 'completed_early'
 
-"""
-Kta te dyja pse jan gabim edhe si me i ba aktive 
-"""
-    # @api.depends('created_for')
-    # def check_user(self):
-    #     for rec in self:
-    #         if rec.created_for.user_id != self.env.user.id:
-    #             raise UserError('You are not allowed to perform this task')
-    #
-    # @api.depends('finish_date')
-    # def finish_date(self):
-    #     for rec in self:
-    #         if not rec.finish_date:
-    #             rec.status = 'in_progress'
+    @api.constrains('finish_date')
+    def check_user(self):
+        """
+        Constraint that prevent anyone to enter the finish date except the person who the task is for
+        """
+        for rec in self:
+            if rec.finish_date:
+                if rec.created_for.user_id != self.env.user:
+                    raise UserError(f'You are not allowed to perform this task, only {rec.created_for.name} is allowed to !')
 
+    @api.constrains('status')
+    def status_lock(self):
+        """
+        After setting this to one of the statuses it cannot be changed anymore!
+        """
+        for rec in self:
+            if rec._origin.status in ['completed', 'completed_delayed', 'completed_early']:
+                if rec._origin.finish_date != rec.finish_date:
+                    raise UserError(
+                        'This task is already completed. If you need modifications please contact the administrator to make changes.')
 
+    @api.constrains('planned_finish_date', 'finish_date')
+    def restriction_date(self):
+        for rec in self:
+            if rec.planned_finish_date and rec.planned_finish_date < rec.starting_date:
+                raise UserError('The planned finish date cannot be in the past!')
+            if rec.finish_date and rec.finish_date < rec.starting_date:
+                raise UserError('The finish date cannot be before the starting date!')
+            """
+            Why it cant be :
+            ```
+            @api.constrains('planned_finish_date', 'finish_date')
+            def restriction_date(self):
+            for rec in self:
+                if rec.planned_finish_date or rec.finish_date < rec.starting_date:
+                    raise UserError('The task cant be arranged for the past, it should be in the future')
+            
+            """
 
 class Teacher(models.Model):
     _inherit = 'teacher.teacher'
