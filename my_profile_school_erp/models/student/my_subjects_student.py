03142dfcee7ea5a496967db227a60ef1631d99ef
enes.elezi <enes.elezi@commprog.com>
2026-04-15 17:15:30 +0200

    Commit #8 (04/15/2026),Added fields

--- my_profile_school_erp/models/student/my_subjects_student.py
+++ my_profile_school_erp/models/student/my_subjects_student.py
@@ -6,22 +6,25 @@ class MySubjects(models.Model):
     _rec_name = 'name'
 
     user_id = fields.Many2one(comodel_name='res.users')
-    student_id = fields.Many2one(comodel_name='students.students',compute='_compute_student_id')
+    student_id = fields.Many2one(comodel_name='students.students',compute='autofill_fields')
     subject_id = fields.Many2one(comodel_name='subject.subject')
 
+    #To be shown in Form view
+    faculty_name = fields.Selection(related='student_id.faculty')
+    year = fields.Selection(related='student_id.year')
+
+
+
     name = fields.Char(related='student_id.name')
-    faculty_name = fields.Selection(related='subject_id.faculty')
-    year = fields.Selection(related='subject_id.year')
     type = fields.Selection( related='subject_id.type')
     credits = fields.Integer(related='subject_id.credits')
     semester = fields.Selection(related='subject_id.semester')
     sub_seq = fields.Char(related='subject_id.sequence')
 
     @api.onchange('user_id')
-    def _compute_student_id(self):
+    def autofill_fields(self):
         """
         Autofill the fields
         """
-        logged_user = self.env.user.id
         for rec in self:
-            rec.student_id = self.env['students.students'].search([('user_id', '=', logged_user)], limit=1).id
+            rec.student_id = self.env['students.students'].search([('user_id', '=', self.env.user.id)], limit=1)
