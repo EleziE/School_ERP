de64497d2607c69f53894938bd610563b75fc845
enes.elezi <eneselezi2000@gmail.com>
2026-04-23 23:32:25 +0200

    HM-Commit #1 (04/23/2026), Working with wizards and reportlab (Print finance doesnt work )

--- students_school_erp/wizards/get_students.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models
-
-class Get_Students(models.TransientModel):
-    _name = 'get.students'
-
-    student_id = fields.Many2one(comodel_name='students.students')
-    dob = fields.Date(related='student_id.dob')
-
-# Still not working ofc is not made yet
